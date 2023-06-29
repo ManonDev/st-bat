14c8d4702b9ff75aca9bedecc4a85fe05cb3e1d4
Manon Bayer <manonbayer.pro@gmail.com>
2023-06-29 16:01:56 +0200

    move files

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="../design/sass_css/defaults.css" rel="stylesheet">
+        <link href="./design/sass_css/defaults.css" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
@@ -13,7 +13,7 @@
     </head>
     <body>
         <div class="container-fluid" id="main">
-         <?PHP include '../src/header.html' ?>
+         <?PHP include 'header.html' ?>
         </div>
         <script type="module" src="../node_modules/bootstrap/"></script>
  
